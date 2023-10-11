e50d462807f2bc2c09b1c1bfebdb194b11187dba
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-10-11 17:37:36 +0300

    Initial Commit

---
+++ CustomAudioPlayer/CustomAudioPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAudioPlayerApp.swift
+//  CustomAudioPlayer
+//
+//  Created by Yunus Emre Taşdemir on 11.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomAudioPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
